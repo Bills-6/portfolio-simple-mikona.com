f3804efcad9e68fec4c854ed83fbe816184ea0a7
Bills-6 <conutconut6@gmail.com>
2025-09-10 03:06:36 +0700

    update: add script hamburger and nav-list +1 issue

--- components/hamburger.js
+++ components/hamburger.js
@@ -1,2 +1,20 @@
 const hamburgerButton = document.getElementById("hamburger-button");
-const navbar = document.getElementById("navbar");
+const navbarList = document.getElementById("nav-list");
+
+hamburgerButton.addEventListener("click", function() {
+	if (navbarList.getAttribute("aria-expanded") === "false") {
+		callNav("open");
+	} else callNav("closed");
+});
+
+function callNav(e) {
+	if (e === "open") {
+		navbarList.setAttribute("aria-expanded", true);
+		navbarList.classList.replace("right-[-200%]", "right-0");
+		hamburgerButton.classList.add("isActive");
+	} else {
+		navbarList.setAttribute("aria-expanded", !true);
+		navbarList.classList.replace("right-0" ,"right-[-200%]");
+		hamburgerButton.classList.remove("isActive");
+	}
+} 
