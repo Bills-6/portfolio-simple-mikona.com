480727fdf3b2720d23d8c0ca06f3045af306ff16
Bills-6 <conutconut6@gmail.com>
2025-09-10 12:55:16 +0700

    update: add bg white to bar

--- components/hamburger.js
+++ components/hamburger.js
@@ -7,14 +7,17 @@ hamburgerButton.addEventListener("click", function() {
 	} else callNav("closed");
 });
 
+const bars = document.querySelectorAll("#hamburger-button .bar");
 function callNav(e) {
 	if (e === "open") {
 		navbarList.setAttribute("aria-expanded", true);
 		navbarList.classList.replace("right-[-200%]", "right-0");
 		hamburgerButton.classList.add("isActive");
+		bars.forEach(bar => bar.style.background = "white");
 	} else {
 		navbarList.setAttribute("aria-expanded", !true);
 		navbarList.classList.replace("right-0" ,"right-[-200%]");
 		hamburgerButton.classList.remove("isActive");
+		bars.forEach(bar => bar.style.background = "");
 	}
 } 
